91caf1706ae9b09d7840ccea73d68de0df13bafe
RealBaty <realbaty@gmail.com>
2023-04-17 19:21:45 +0300

    Bag fix

--- app/src/main/java/com/hh/contractstestsadmin/resource/StatusResource.java
+++ app/src/main/java/com/hh/contractstestsadmin/resource/StatusResource.java
@@ -25,7 +25,7 @@ public class StatusResource {
     @Path("services")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getServices(@QueryParam("standId") Long standId){
+    public Response getServices(@QueryParam("standId") String standId){
         return Response.ok(new ArrayList<ServiceDto>()).build();
     }
 
