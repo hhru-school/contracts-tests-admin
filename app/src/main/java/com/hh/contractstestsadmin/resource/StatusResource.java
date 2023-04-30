b29000102cee33a68b27174008d106cbea635db5
RealBaty <realbaty@gmail.com>
2023-04-30 17:32:05 +0300

    Api refactor

--- app/src/main/java/com/hh/contractstestsadmin/resource/StatusResource.java
+++ app/src/main/java/com/hh/contractstestsadmin/resource/StatusResource.java
@@ -4,11 +4,10 @@ import com.hh.contractstestsadmin.exception.StandNotFoundException;
 import com.hh.contractstestsadmin.service.StatusService;
 
 import javax.inject.Inject;
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -33,10 +32,10 @@ public class StatusResource {
     }
   }
 
-  @Path("services")
+  @Path("/stand/{standName}/services")
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public Response getServices(@NotNull @QueryParam("standName") String standName) {
+  public Response getServices(@PathParam("standName") String standName) {
     try {
       return Response.ok(statusService.getServices(standName)).build();
     } catch (StandNotFoundException exception) {
