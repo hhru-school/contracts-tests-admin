0b30a0c59426a2d1cc5afe9e36f912c620d98ab8
Valentine Gavrilenko <gavrilenko.valantine@yandex.ru>
2023-05-16 11:12:17 +0300

    add Download button to ServicesListItem

--- frontend/src/components/Services/ListItem.tsx
+++ frontend/src/components/Services/ListItem.tsx
@@ -1,3 +1,5 @@
+import { ReactComponent as DownloadIcon } from './img/download.svg';
+import { Button, Col, ListGroupItem, Row } from 'reactstrap';
 import { Service } from './types/Service';
 
 export type ServicesListItemProps = {
@@ -5,32 +7,47 @@ export type ServicesListItemProps = {
 };
 
 export const ServicesListItem: React.FC<ServicesListItemProps> = ({ item }) => {
+    const { name, version, isConsumer, isProducer, expectationPublishDate, schemaPublishDate } =
+        item;
+
     return (
-        <div className="list-group-item list-group-item-action d-flex gap-3 py-3 justify-content-between">
-            <div>
-                <h6 className="mb-0">{item.name}</h6>
-                <p className="mb-0 opacity-75">{item.version}</p>
-            </div>
-            <div className="d-flex align-items-center gap-2" style={{ minWidth: '25%' }}>
-                {item.isConsumer && (
-                    <>
-                        <p className="mb-0 opacity-75">Consumer</p>
-                        <small className="opacity-50 text-nowrap">
-                            {new Date(item.expectationPublishDate).toLocaleDateString('ru-RU')}
-                        </small>
-                    </>
-                )}
-            </div>
-            <div className="d-flex align-items-center gap-2" style={{ minWidth: '25%' }}>
-                {item.isProducer && (
-                    <>
-                        <p className="mb-0 opacity-75">Producer</p>
-                        <small className="opacity-50 text-nowrap">
-                            {new Date(item.schemaPublishDate).toLocaleDateString('ru-RU')}
-                        </small>
-                    </>
-                )}
-            </div>
-        </div>
+        <ListGroupItem>
+            <Row>
+                <Col md={4}>
+                    <h6 className="mb-0">{name}</h6>
+                    <p className="mb-0 opacity-75">{version}</p>
+                </Col>
+                <Col md={4}>
+                    {isConsumer && (
+                        <div className="d-flex align-items-center justify-content-center gap-3">
+                            <div>
+                                <p className="mb-0 opacity-75">Consumer</p>
+                                <small className="opacity-50 text-nowrap">
+                                    {new Date(expectationPublishDate).toLocaleDateString('ru-RU')}
+                                </small>
+                            </div>
+                            <Button color="primary" outline>
+                                <DownloadIcon />
+                            </Button>
+                        </div>
+                    )}
+                </Col>
+                <Col md={4}>
+                    {isProducer && (
+                        <div className="d-flex align-items-center justify-content-center gap-3">
+                            <div>
+                                <p className="mb-0 opacity-75">Producer</p>
+                                <small className="opacity-50 text-nowrap">
+                                    {new Date(schemaPublishDate).toLocaleDateString('ru-RU')}
+                                </small>
+                            </div>
+                            <Button color="primary" outline>
+                                <DownloadIcon />
+                            </Button>
+                        </div>
+                    )}
+                </Col>
+            </Row>
+        </ListGroupItem>
     );
 };
