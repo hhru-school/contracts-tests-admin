c84fd34e315e5911c78702993194e79ef2f3aeb2
Valentine Gavrilenko <gavrilenko.valantine@yandex.ru>
2023-05-17 14:36:48 +0300

    wrap pages outlet into Container

--- frontend/src/pages/Statuses/index.tsx
+++ frontend/src/pages/Statuses/index.tsx
@@ -2,20 +2,18 @@ import { ServicesContainer } from 'components/Services/Container';
 import { ValidationHistory } from 'components/ValidationHistory';
 import { AppContext } from 'context/AppContext';
 import { useContext } from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 
 export const StatusesPage: React.FC = () => {
     const { standName } = useContext(AppContext);
     return (
-        <Container>
-            <Row className="pt-3">
-                <Col md={10}>
-                    <ServicesContainer standName={standName} />
-                </Col>
-                <Col>
-                    <ValidationHistory />
-                </Col>
-            </Row>
-        </Container>
+        <Row>
+            <Col md={10}>
+                <ServicesContainer standName={standName} />
+            </Col>
+            <Col>
+                <ValidationHistory />
+            </Col>
+        </Row>
     );
 };
