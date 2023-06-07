be1f5e432c788761c485272cb0a2f50eead904db
ziyakaev <vip.ziyakaev@gmail.com>
2023-06-07 17:31:06 +0300

    HH-127 add encode file

--- app/src/main/java/com/hh/contractstestsadmin/service/mapper/ServiceRelationMapper.java
+++ app/src/main/java/com/hh/contractstestsadmin/service/mapper/ServiceRelationMapper.java
@@ -4,6 +4,8 @@ import com.hh.contractstestsadmin.dto.api.ServicesRelationDto;
 import com.hh.contractstestsadmin.model.Service;
 import com.hh.contractstestsadmin.model.ServiceRelation;
 import com.hh.contractstestsadmin.model.ServiceType;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 public class ServiceRelationMapper {
@@ -13,15 +15,17 @@ public class ServiceRelationMapper {
     ServicesRelationDto servicesRelationDto = new ServicesRelationDto();
     if (serviceRelation.getProducer() != null) {
       Service producer = serviceRelation.getProducer();
-      servicesRelationDto.setProducer(
-          ServiceMapper.map(producer, producer.getSchemaLink(), ServiceType.PRODUCER)
+      servicesRelationDto.setProducer(ServiceMapper.map(producer,
+              URLEncoder.encode(producer.getSchemaLink(), StandardCharsets.UTF_8), ServiceType.PRODUCER
+          )
       );
     }
 
     if (serviceRelation.getConsumer() != null) {
       Service consumer = serviceRelation.getConsumer();
-      servicesRelationDto.setConsumer(
-          ServiceMapper.map(consumer, consumer.getExpectationLink(), ServiceType.CONSUMER)
+      servicesRelationDto.setConsumer(ServiceMapper.map(consumer,
+              URLEncoder.encode(consumer.getExpectationLink(), StandardCharsets.UTF_8), ServiceType.CONSUMER
+          )
       );
     }
     servicesRelationDto.setErrorCount(serviceRelation.getErrorCount());
