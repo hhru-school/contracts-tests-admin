4a5faded30217413113b334f46eefc789008ce44
ziyakaev <vip.ziyakaev@gmail.com>
2023-06-22 12:54:57 +0300

    HH-204 have filter for error level

--- app/src/main/java/com/hh/contractstestsadmin/service/builder/ValidationBuilder.java
+++ app/src/main/java/com/hh/contractstestsadmin/service/builder/ValidationBuilder.java
@@ -32,7 +32,7 @@ public class ValidationBuilder {
             .map(this::buildServicesRelationDto)
             .toList()
     );
-    validationWithRelationsDto.setErrorCount(serviceRelations.size());
+
     return validationWithRelationsDto;
   }
 
